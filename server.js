fbb58031e03f2bb2bee62b828bdc69044918dbc4
Jordan McCollam <jordy.mccollam@gmail.com>
2019-10-16 20:54:08 -0500

    created API routes for reservations and waitlists

--- server.js
+++ server.js
@@ -2,21 +2,31 @@
 // Dependancies
 var express = require("express");
 var path = require("path");
+var reservations = require("./reservations");
+var waitList = require("./waitlist");
 
 // Express app config
 var app = express();
 var port = 3000;
 
 // data parsing config
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
 
 // ROUTES ===============================================
+app.get("/api/tables", function (req, res) {
+    return res.json(reservations);
+})
 
+app.get("/api/waitlist", function (req, res) {
+    return res.json(waitList);
+})
 
 
 // LISTENING ============================================
-app.listen(port, function() {
+app.listen(port, function () {
     console.log("App listening on PORT: " + port);
 })
